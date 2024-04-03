1c5586bf4ec3b0c79e85524f0ea38550bd32da51
fuad888 <ffranco14@uan.edu.co>
2024-04-03 16:30:54 -0500

    Actualizacion

--- modelo/requerimientos_carros.py
+++ modelo/requerimientos_carros.py
@@ -9,13 +9,12 @@ class EstandaresCarros:
         self._load_model()
    
     def predecir_precio(self, year, mileage, state, make, model):   
-        self.validar_entrada_year(year, mileage, state, make, model)    
+        self.validar_entrada_year(year, mileage, state, make, model)
         price = self.modelo_carro.predict(year, mileage, state, make, model)
         return price
     
     def _load_model(self):
         self.__model = joblib.load(os.path.dirname(__file__) + '\\training\\predictor_precios_model.pkl')  
-        pass
     
     def validar_entrada_year(self, year, mileage, state, make, model):
         if not isinstance(year, int) or year < 1900 or year > 2024:
